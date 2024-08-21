ed62d01940b7a211397874535b4a7fda7b5b7fbe
qiqzhao <bme_ritter@foxmail.com>
2024-08-21 15:38:30 +0800

    chore: init project

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import { Component } from 'react'
+
+import type { PropsWithChildren } from 'react'
+
+import './app.scss'
+
+class App extends Component<PropsWithChildren> {
+
+  componentDidMount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  // this.props.children 是将要会渲染的页面
+  render() {
+    return this.props.children
+  }
+}
+
+
+export default App
